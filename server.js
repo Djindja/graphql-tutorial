370b845cafdba870af0e8a4b278df587e7572c8b
Stefan <stefansdjindjic@gmail.com>
2019-02-25 18:58:31 +0100

    graphql apollo example

---
+++ server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import {graphqlExpress, graphiqlExpress}  from 'apollo-server-express';
+import bodyParser from 'body-parser';
+import schema from './schema.js';
+import mongoose from 'mongoose';
+const server = express();
+
+server.use('/graphiql', graphiqlExpress({
+    endpointURL: "/graphql"
+}));
+
+mongoose.connect('mongodb://localhost/graphqlTutorial', {
+    useMongoClient: true
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+    console.log('Connection to DB was successful');
+});
+
+server.use('/graphql', bodyParser.json(), graphqlExpress({schema}));
+
+server.listen(4000, () => {
+    console.log('Listening on port 4000');
+});
